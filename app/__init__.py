60c034a5660ae717d065d5e50b4e232edd4996de
Aaron Contreras <acontreras20@stuy.edu>
2021-11-02 21:32:28 -0400

    added basic registration page

--- app/__init__.py
+++ app/__init__.py
@@ -13,62 +13,39 @@ import os #for secret_key
 app = Flask(__name__)    #create Flask object
 app.secret_key = os.urandom(32)
 
-
-'''
-trioTASK:
-~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
-...read for understanding all of the code below.
-Some will work as written; other sections will not. Can you predict which?
-Devise some simple tests you can run to "take apart this engine," as it were.
-Execute your tests. Process results.
-PROTIP: Insert your own in-line comments wherever they will help your future self and/or current teammates understand what is going on.
-'''
-
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
     #print(session)
     if session.get("name"):
         return loggedIn() #remembers when you're logged in
-    # print("\n\n\n")
-    # print("***DIAG: this Flask obj ***")
-    # print(app) #prints app
-    # print("***DIAG: request obj ***")
-    # print(request) #prints link and methods
-    # print("***DIAG: request.args ***")
-    # print(request.args) #prints empty dictionary since there have been no arguments inputted
-    # #print("***DIAG: request.args['username']  ***") <- only works after submitting form
-    # #print(request.args['username'])
-    # print("***DIAG: request.headers ***")
-    # print(request.headers)  #prints various information
-    return render_template( 'login.html', error = '' ) #webpage is just the login.html file in templates
+    
+    return render_template('base.html')
 
 
-@app.route("/auth") # , methods=['GET', 'POST'])
+@app.route("/auth", methods=['GET', 'POST'])
 def authenticate():
-    if(request.args['username'] == ''): #empty username
+    if(request.form['username'] == ''): #empty username
         return Error("No username inputted. Login again. ")
-    if(request.args['password'] == ''): #empty password
+    if(request.form['password'] == ''): #empty password
         return Error("No password inputted. Login again.")
-    if (request.args['username'] != "user") or (request.args['password'] != "password"):
-        if request.args['username'] != "Snaps": #incorrect Username
-            return Error("Incorrect Username! Login again. ")
+    if (request.form['username'] != "user") or (request.form['password'] != "password"):
+        #if request.form['username'] != "Snaps": #incorrect Username
+            #return Error("Incorrect Username! Login again. ")
         return Error("Incorrect Password! Login again." )
-    #print("\n\n\n")
-    #print("***DIAG: this Flask obj ***")
-    #print(app) #same thing as disp_loginpage()
-    #print("***DIAG: request obj ***")
-    #print(request) #link of page (with the inputs from the form)
-    #print("***DIAG: request.args ***")
-    #print(request.args) #not empty anymore
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username']) #prints the username that you inputted
-    #print("***DIAG: request.headers ***")
-    #print(request.headers) #same as disp_loginpage
+  
     m = request.method #either get or post
-    session["name"] = request.args['username'] #inputs cookies
-    session["password"] = request.args['password'] #inputs cookies
+    session["name"] = request.form['username'] #inputs cookies
+    session["password"] = request.form['password'] #inputs cookies
     #print(request.form['username'])
     return render_template('response.html', username = session["name"], method = m)
+
+@app.route("/login" , methods = ['GET', 'POST'])
+def disp_login():
+    return render_template('login.html', error = '')
+
+@app.route("/register" , methods = ['GET', 'POST'])
+def disp_register():
+    return render_template('register.html')
 
 @app.route("/out")
 def out(): #logging out
@@ -77,11 +54,11 @@ def out(): #logging out
         session.pop("name")
         session.pop("password")
     #print(len(session))
-    return render_template('login.html', error = '') #goes back to login page
+    return render_template('base.html', error = '') #goes back to login page
 
 @app.route("/empty")
 def Error(message): #inputted message for optimiziation
-    return render_template('login.html', error = message) #message depends on the error checked in authenticate
+    return render_template('base.html', error = message) #message depends on the error checked in authenticate
 
 @app.route("/in")
 def loggedIn():
