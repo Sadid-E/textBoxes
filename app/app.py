ea0d1f498120a4dc9c8f90e3e08cd05604e8d53a
gavin-pog <gavinjhmcginley@gmail.com>
2021-11-10 23:34:59 -0500

    editing other people's stories

--- app/app.py
+++ app/app.py
@@ -101,22 +101,110 @@ def out(): #logging out
 def loggedIn():
 	return render_template('response.html', username = session["name"], method = "GET", error="")
 
+@app.route("/findStories", methods=['GET', 'POST'])
+def newstorylist():
+	db = sqlite3.connect("story.db")
+	c = db.cursor()
+	c.execute("SELECT storiescontributed FROM accounts WHERE username='"+session['name']+"';")
+	new = c.fetchone()[0]
+	contributed = new.split("/")[1:-1]
+	c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+	total=c.fetchall();
+	unused=[]
+	for i in total:
+		if (i[0] not in contributed and i[0] != "accounts"):
+			unused.append(i[0])
+	titles=[]
+	lastentry=[]
+	poster=[]
+
+	for i in unused:
+		c.execute("SELECT entrytext FROM '"+i+"' WHERE entrynum=0")
+		store=c.fetchone()[0];
+		titles.append(store)
+
+		c.execute("SELECT entrytext FROM '" + i + "' WHERE entrynum=-1")
+		store = c.fetchone()[0];
+		lastentry.append(store)
+
+		c.execute("SELECT user FROM '" + i + "' WHERE entrynum=-1")
+		store = c.fetchone()[0];
+		poster.append(store)
+	db.commit()
+	db.close()
+	num = len(poster)
+	return render_template('recentStories.html',titles=titles,lastentry=lastentry,poster=poster,num=num, buttons=unused)
+
+@app.route("/editstory", methods=['GET', 'POST'])
+def entrypage():
+	db = sqlite3.connect("story.db")
+	c = db.cursor()
+	i=request.form["toedit"]
+	print(i)
+	c.execute("SELECT entrytext FROM '" + i + "' WHERE entrynum=-1")
+	lastentry = c.fetchone()[0];
+
+	c.execute("SELECT entrytext FROM '" + i + "' WHERE entrynum=0")
+	title = c.fetchone()[0];
+
+	c.execute("SELECT user FROM '" + i + "' WHERE entrynum=-1")
+	lastuser = c.fetchone()[0];
+
+	c.execute("SELECT user FROM '" + i + "' WHERE entrynum=0")
+	origin = c.fetchone()[0];
+	db.commit()
+	db.close()
+	#don't forget to address this later
+	session['target']=i
+	return render_template("editstory.html", title=title, origin=origin, lastentry=lastentry, poster=lastuser, error="", entry="New Entry" )
+
+@app.route("/updatestory", methods=['GET', 'POST'])
+def updatestory():
+	k=request.form['newentry']
+	i = session['target']
+	session.pop('target')
+	wordcount=len(k.split(" "))
+	q="Your entry is "+str(wordcount)+" words long. Please reduce it to less than 200."
+	if (wordcount>200):
+		return render_template("editstory.html", title=title, origin=origin, lastentry=lastentry, poster=lastuser, error=q, entry=k )
+	else:
+		db = sqlite3.connect("story.db")
+		c = db.cursor()
+		c.execute("SELECT entrynum from "+i)
+		nums=c.fetchall()
+		top=1;
+		for en in nums:
+			if(en[0]>top):
+				top=en[0]
+		top=top+1
+		c.execute('INSERT INTO ' + i + ' (entrynum, entrytext, user) VALUES(?,?,?)',
+				  (top, k, session["name"]))
+		c.execute("UPDATE "+i+" SET entrytext='" + k + "',user= '" + session['name'] + "' WHERE entrynum=-1")
+
+		c.execute("SELECT storiescontributed FROM accounts WHERE username='" + session['name'] + "'")
+		new = c.fetchone()[0] + i + "/"
+		c.execute("UPDATE accounts SET storiescontributed='" + new + "' WHERE username='" + session['name'] + "'")
+
+		db.commit()
+		db.close()
+		return render_template('response.html', username = session["name"], method = "GET", error="")
 @app.route("/makestory", methods=['GET', 'POST'])
 def editpage():
-	return render_template('createstory.html',error="")
+	return render_template('createstory.html',error="", entry="First Entry")
 
 @app.route("/poststory", methods=['GET', 'POST'])
 def poststory():
 	title=request.form["storytitle"]
+	entry=request.form['firstentry']
 	if (title=="" or title.isspace()): 
-		return render_template('createstory.html',error="Please enter a title.")
+		return render_template('createstory.html',error="Please enter a title.",entry=entry)
 	if not title.replace(" ","").isalnum(): # checks if title only includes alphanumeric characters
-		return render_template('createstory.html',error="Please make sure the title only includes alphanumeric characters.")
+		return render_template('createstory.html',error="Please make sure the title only includes alphanumeric characters.",entry=entry)
 	db = sqlite3.connect("story.db")
 	c = db.cursor()
 	k=c.execute("SELECT * FROM sqlite_master WHERE type='table' AND name=:n;", {"n":title.replace(" ","").lower()}) # checks for duplicate titles
-	print(c.fetchall())
-	if c.fetchall()!=[]:
+
+	if c.fetchall():
 		db.commit()
 		db.close()
 		return render_template('createstory.html',error="Title already in use. Please pick another one")
@@ -125,6 +213,7 @@ def poststory():
 		c.execute("CREATE TABLE "+title.replace(" ","").lower()+"(entrynum INTEGER,entrytext TEXT,user TEXT);")
 		c.execute('INSERT INTO '+title.replace(" ","").lower()+' (entrynum, entrytext, user) VALUES(?,?,?)',(0,title,session["name"]))
 		c.execute('INSERT INTO ' + title.replace(" ","").lower() + ' (entrynum, entrytext, user) VALUES(?,?,?)', (1, text, session["name"]))
+		c.execute('INSERT INTO ' + title.replace(" ", "").lower() + ' (entrynum, entrytext, user) VALUES(?,?,?)',(-1, text, session["name"]))
 		c.execute("SELECT storiescontributed FROM accounts WHERE username='" + session['name'] + "'")
 		new= c.fetchone()[0]+title.replace(" ","").lower()+"/"
 		c.execute("UPDATE accounts SET storiescontributed='"+new+"' WHERE username='" + session['name'] + "'")
